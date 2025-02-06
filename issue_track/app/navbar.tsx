882eedc8dcf6f7fad4fb4ec40bd03fe5d6e93cc5
sanuanfan <sanuanfan8535@gmail.com>
2025-02-06 13:46:42 +0530

    navbar styling and working

--- issue_track/app/navbar.tsx
+++ issue_track/app/navbar.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
+// import { text } from 'stream/consumers';
+import classNames from 'classnames';
 
 
 const NavBar = () => {
+    const currentPath = usePathname();
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'issues', href: '/issues' }
@@ -13,7 +19,14 @@ const NavBar = () => {
             <Link href="/"><AiFillBug /></Link>
             <ul className='flex space-x-6 '>
                 {links.map(link =>
-                    <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}
+                    <Link key={link.href}
+                        className={classNames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
+                        href={link.href}>
+                        {link.label}
                     </Link>
                 )}
             </ul>
